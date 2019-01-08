d487c9f95fde3567764714113ed1807f3d35fa05
Andres.C <andrescabrera@pursuit.org>
2019-01-08 00:25:41 -0500

    nasa logo

--- forms1/src/forms.js
+++ forms1/src/forms.js
@@ -38,82 +38,108 @@ class Form extends Component {
     console.log(this.state);
     return (
       <>
-        <h1> Mission to Mars Registration Form </h1>
+        <div class = 'image'>
+          <img className="nasa" src={"https://seeklogo.com/images/N/NASA-logo-9411797223-seeklogo.com.png"} alt =""/>
+        </div>
 
-        <form onChange={this.handleChange}>
-          <label class = "text" htmlFor="name"> What is your name ? </label>
-          <input
-            onChange={this.handleChange}
-            type="text"
-            placeholder="A name"
-            name="name"
-            value={this.state.name}
-            id="name"
-          />
-            <br />
+        <div class="form">
+          <h1> Mission to Mars Registration Form </h1>
 
-          <br />
-          <label class = "text" htmlFor="birthday"> What is your date of birth ? </label>
+          <form onChange={this.handleChange}>
+            <label class="text" htmlFor="name">
+              {" "}
+              What is your name ?{" "}
+            </label>
 
-          <input
-            type="date"
-            id="start"
-            name="trip-start"
-            min="1900-01-01"
-            max="2019-01-07"
-          />
-            <br />
-              <br />
-
-
-          <label class = "text" htmlFor="email"> Email </label>
-          <input
-            onChange={this.handleChange}
-            type="text"
-            placeholder="A email"
-            name="email"
-            value={this.state.email}
-            id="email"
-          />
-
-
-
-          <select  id ="favorite" name="favorite" value={this.state.favorite}>
-            <option value="omnivore"> Omnivore </option>
-            <option value="vegetarian"> Vegetarian </option>
-            <option value="vegan"> Vegan </option>
-          </select>
-
-          <br />
-            <br />
-          <div id="country">
-            <label class = "text" htmlFor="country"> What is your country of origin?</label>
-
-            <select
-              placeholder="Select Country"
-              name="country"
-              value={this.state.country}
-              id = "country"
-            >
-              {this.populate()}
-            </select>
-          </div>
-  <br />
-          <div id="whytext">
-            <label  class = "text" htmlFor="why">Why do you want to be a Mars explorer?</label>
             <input
               onChange={this.handleChange}
               type="text"
-              placeholder="Enter Text Here"
-              name="why"
-              value={this.state.why}
-              id="why"
+              placeholder="A name"
+              name="name"
+              value={this.state.name}
+              id="name"
             />
-          </div>
-        </form>
+            <br />
+            <br />
+
+            <label class="text" htmlFor="birthday">
+              {" "}
+              What is your date of birth ?{" "}
+            </label>
+
+            <input
+              type="date"
+              id="start"
+              name="trip-start"
+              min="1900-01-01"
+              max="2019-01-07"
+            />
+
+            <br />
+            <br />
+
+            <label class="text" htmlFor="email">
+              {" "}
+              Email{" "}
+            </label>
+
+            <input
+              onChange={this.handleChange}
+              type="text"
+              placeholder="A email"
+              name="email"
+              value={this.state.email}
+              id="email"
+            />
+
+            <select id="favorite" name="favorite" value={this.state.favorite}>
+              <option value="omnivore"> Omnivore </option>
+              <option value="vegetarian"> Vegetarian </option>
+              <option value="vegan"> Vegan </option>
+            </select>
+
+            <br />
+            <br />
+
+            <div id="country">
+              <label class="text" htmlFor="country">
+                {" "}
+                What is your country of origin?
+              </label>
+
+              <select
+                placeholder="Select Country"
+                name="country"
+                value={this.state.country}
+                id="country"
+              >
+                {this.populate()}
+              </select>
+            </div>
+
+            <br />
+
+            <div id="whytext">
+              <label class="text" htmlFor="why">
+                Why do you want to be a Mars explorer?
+              </label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                placeholder="Enter Text Here"
+                name="why"
+                value={this.state.why}
+                id="why"
+              />
+            </div>
+          </form>
+
           <br />
-        <div id ="submit">
-        <button>Submit</button>
+
+          <div id="submit">
+            <button>Submit</button>
+          </div>
+
         </div>
       </>
     );
